b8f8fc4d15efe274b29d14657b8fd2f167236efc
Alejandro Anzola <anzola95avila@gmail.com>
2022-10-29 23:39:14 -0500

    adds test for client base methods

---
+++ client_test.go
@@ -0,0 +1,34 @@
+package splitwise
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_IfTokenIsDefinedItShouldBeInHeader(t *testing.T) {
+	const testToken = "testtoken"
+
+	client, cancel := testClientWithHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader != fmt.Sprintf("Bearer %s", testToken) {
+			assert.Failf(t, "authorization header is not correct", "authorization header is [%s], should be [Bearer %s]", authHeader, testToken)
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer cancel()
+
+	ctx := context.Background()
+
+	client.Token = testToken
+
+	res, err := client.do(ctx, http.MethodGet, "/generic", nil, nil)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+}
